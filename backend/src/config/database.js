5a881de0abb797e59eec9c0bb1bb1d1c047fff67
cristian212010 <cristiancamilo212010@gmail.com>
2023-07-31 21:54:22 -0500

    feat: :card_file_box: Conexion bs
    se realizo la conexion de la base de datos con la dependencia mongoose

---
+++ backend/src/config/database.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const dbConection = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+    } catch (error) {
+        console.log(error);
+        throw new Error('DB CANNOT CONNECT');
+    };
+};
+
+export default dbConection;
