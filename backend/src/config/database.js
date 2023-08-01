c1c4ee6169cfa04299e143ab86cc8dc08ba214b6
cristian212010 <cristiancamilo212010@gmail.com>
2023-07-31 22:06:30 -0500

    feat: :card_file_box: Conexion bd

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const dbConection = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI,{
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
