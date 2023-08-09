524379bc4065e312e7528651d643cacf2b3b4628
cristian212010 <cristiancamilo212010@gmail.com>
2023-08-09 06:00:15 -0500

    feat: :sparkles: Se agrego el generador de Json Web Token
    se agrego el Json Web Token para sus respectivas validaciones

---
+++ backend/src/helpers/generate.JWT.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+
+const generateJWT =  (uid= '') =>{
+    return new Promise ((resolve, reject)=>{
+        const payload = {uid};
+
+        jwt.sign(payload,process.env.SECRET_OR_PRIVATE_KEY, {
+            expiresIn : '4h'
+        }, (err, token)=>{
+            if (err){
+                console.log(err);
+                reject ('No se pudo generar el JSON WEB TOEKN')
+            } else {
+                resolve (token)
+            }
+        })
+    })
+}
+
+export default generateJWT;
